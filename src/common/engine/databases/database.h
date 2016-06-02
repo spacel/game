86ef2e8dc2e6c1cc1aaa9341cf6bc555894f2071
Loic Blot <loic.blot@unix-experience.fr>
2016-06-02 22:09:54 +0200

    Prepare basic database.h virtual class

---
+++ src/common/engine/databases/database.h
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Spacel game.
+ *
+ * Copyright 2016, Loic Blot <loic.blot@unix-experience.fr>
+ *
+ * Spacel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Spacel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Spacel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+namespace spacel {
+
+namespace engine {
+
+class Database
+{
+public:
+	Database() {}
+	virtual ~Database() {}
+private:
+	virtual bool open() = 0;
+	virtual void updateSchema() = 0;
+	virtual bool close() = 0;
+};
+}
+}
