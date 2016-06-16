47bd19768fb255a7cf488b3473ffdef56adadfa6
Loic Blot <loic.blot@unix-experience.fr>
2016-06-16 20:23:35 +0200

    Try to add unittests to travis

--- util/travis/script.sh
+++ util/travis/script.sh
@@ -6,5 +6,5 @@ cmake -DURHO3D_SSE=0 -DURHO3D_SAMPLES=0 -DURHO3D_MINIDUMPS=0 ..
 make -j2
 
 cd ../../../build/
-cmake -D URHO3D_HOME=$(pwd)/../lib/Urho3D/build/ ..
+cmake -D URHO3D_HOME=$(pwd)/../lib/Urho3D/build/ -DENABLE_UNITTESTS=1 ..
 make -j2
