0ca289930ff2ba15b344ac0b06f56f86c6fe1593
Loic Blot <loic.blot@unix-experience.fr>
2016-08-10 11:04:27 +0200

    Travis should build URHO using c++11, sqlite and remove tools

--- util/travis/script.sh
+++ util/travis/script.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build lib/Urho3D/build
 cd lib/Urho3D/build/
-cmake -DURHO3D_SSE=0 -DURHO3D_SAMPLES=0 -DURHO3D_MINIDUMPS=0 ..
+cmake -DURHO3D_SSE=0 -DURHO3D_SAMPLES=0 -DURHO3D_MINIDUMPS=0 -DURHO3D_C++11=1 -DURHO3D_DATABASE_SQLITE=1 -DURHO3D_TOOLS=0 ..
 make -j2
 
 cd ../../../build/
